21a729e175689e4c35297d9fd74732d941916cf1
Mohd-Ishaq <ishaqmohd116@gmail.com>
2023-01-16 23:12:16 +0530

    created footer

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
 import NavBar from "../organisms/NavBar";
+import Footer from "../organisms/footer/footer"
 
 export default function Layout({ children }){
 return (
     <>
     <NavBar/>
+    <Footer />
     <main>{children}</main>
+
+
     </>
 )
 }
---
+++ components/organisms/footer/footer.jsx
@@ -0,0 +1,13 @@
+import styles from "./footer.module.css"
+
+
+
+export default function footer(){
+    return(
+        <div className={styles.Footer}>
+            <h3>
+                Copyright 2023. All Rights Reserved.
+            </h3>
+        </div>
+    )
+}
